d93adb6aade862cafb1a8e35ef5e717cf5ea05fc
Agnieszka Kaminska <zabula81@o2.pl>
2023-12-18 14:30:30 +0100

    first

--- 01/app.js
+++ 01/app.js
@@ -1,2 +1,13 @@
 const randomNumber = Math.round(Math.random() * 20);
 console.log(randomNumber);
+console.log("-----------");
+
+
+if (randomNumber <= 5) {
+  console.log("Wylosowana liczba jest zbyt mała, aby użyć pętli");
+  return;
+}
+
+for(let i = 5; i <= randomNumber; i++){
+    console.log(i)
+}
